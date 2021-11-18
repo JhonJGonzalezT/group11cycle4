a21947279c2388b9443b7b8be9aea2dfdf41ad3c
Jhon Jairo Gonzalez Torrenegra <jhonjgonzalezt@gmail.com>
2021-11-18 00:18:55 -0500

    sprint completed
    sprint completed

--- js/src.js
+++ js/src.js
@@ -1,35 +1,23 @@
-const calcularDepreciacionNIIF = (precioInicial, precioFinal, vidaUtil,numeroPeriodoAconsultar) => {
-    if (vidaUtil<=0) {
+const calcularDepreciacionNIIF = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+    if ( vidaUtil <= 0 ) {
         return 0;
-    } else {
-        if (numeroPeriodoAconsultar>vidaUtil) {
-            return precioFinal;
-        } else {
-            for (let index = 0; index < numeroPeriodoAconsultar; index++) {
-                var depreciacion = (precioInicial-precioFinal)/vidaUtil;
-            }
-            return depreciacion;
-        }
     }
+    if ( numeroPeriodoAconsultar > vidaUtil ) {
+        return precioFinal;
+    }
+    let annualDepreciation = ( precioInicial - precioFinal ) / vidaUtil;
+    for ( let i = 0; i < numeroPeriodoAconsultar; i++ ) {
+        depreciation = precioInicial - ( numeroPeriodoAconsultar * annualDepreciation );
+    }
+    return depreciation;
 }
 
-const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil,numeroPeriodoAconsultar) => {
-    if (vidaUtil<=0) {
-        return 0;
-    } else {
-        if (numeroPeriodoAconsultar>vidaUtil) {
-            return precioFinal;
-        } else {
-            if (precioInicial<0) {
-                return "error";
-            } else {
-                for (let index = 0; index < numeroPeriodoAconsultar; index++) {
-                    var depreciacion = (precioInicial-precioFinal)/vidaUtil;
-                }
-                return depreciacion;
-            }
-        }
+const calcularDepreciacionNIIFEnDolares = (precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) => {
+    const USDtoCOP = 3778;
+    if ( precioInicial < 0 ) {
+        throw "error"
     }
+    return calcularDepreciacionNIIF(precioInicial, precioFinal, vidaUtil, numeroPeriodoAconsultar) / USDtoCOP;
 }
 
 module.exports.calcularDepreciacionNIIF=calcularDepreciacionNIIF;
